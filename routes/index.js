9b0ac422e9c9ce4bc5a459a1b41712da6e996039
Prawee Wongsa <prawee@hotmail.com>
2022-03-16 15:33:06 +0700

    separate  routes

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+/**
+ * @ Author: Prawee Wongsa (prawee@hotmail.com)
+ * @ Create Time: 2022-03-16 14:56:02
+ * @ Modified by: Prawee@hotmial.com
+ * @ Modified time: 2022-03-16 15:26:33
+ * @ Description:
+ */
+const home = require("./home")
+const profile = require("./profile")
+const logging = require("./logging")
+
+module.exports = {
+    home,
+    profile,
+    logging
+}
