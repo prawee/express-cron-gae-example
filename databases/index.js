5cafd07332f6a175724b5a20c1e3c43a58dbbafc
Prawee Wongsa <prawee@hotmail.com>
2022-03-16 17:25:24 +0700

    make connection with promise

--- databases/index.js
+++ databases/index.js
@@ -2,10 +2,11 @@
  * @ Author: Prawee Wongsa (prawee@hotmail.com)
  * @ Create Time: 2022-03-16 15:10:35
  * @ Modified by: Prawee@hotmial.com
- * @ Modified time: 2022-03-16 15:17:39
+ * @ Modified time: 2022-03-16 16:32:09
  * @ Description:
  */
 const mysql = require("mysql")
+const util = require("util")
 
 let configure = {
     user: process.env.DB_USER,
@@ -19,5 +20,6 @@ if (process.env.APP_ENV === 'development') {
 }
 
 const pool = mysql.createPool(configure)
+pool.query = util.promisify(pool.query)
 
 module.exports = pool
