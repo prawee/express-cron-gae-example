324f002940aa2f2cb6a02f52350fea91e529d456
Prawee Wongsa <prawee@hotmail.com>
2022-03-16 15:33:19 +0700

    separate routes and configure

--- server.js
+++ server.js
@@ -2,26 +2,18 @@
  * @ Author: Prawee Wongsa (prawee@hotmail.com)
  * @ Create Time: 2022-03-08 01:29:25
  * @ Modified by: Prawee@hotmial.com
- * @ Modified time: 2022-03-16 14:23:13
+ * @ Modified time: 2022-03-16 15:27:45
  * @ Description: template for express with cron on gcp
  */
+require("dotenv").config()
+require('@google-cloud/trace-agent').start();
 
 /**
  * instance
  */
-require("dotenv").config()
-require('@google-cloud/trace-agent').start();
 const express = require("express")
-const mysql = require("mysql")
 const app = express()
-const logger = require("./logger/winston")
-// const testLogger = require("./logger/test")
-// const logger = require("./logger")
-
-/**
- * global variable
- */
-let count = 0
+const routes = require("./routes")
 
 /**
  * using middleware
@@ -33,46 +25,9 @@ app.use(express.json())
 /**
  * route
  */
-app.get("/", (req, res) => {
-  res.send("Hello from App Engine")
-})
-
-app.get("/cron", (req, res) => {
-  logger.info('testing info logger')
-  logger.warn('testing warning logger')
-  logger.error('testing error logger')
-  logger.debug('testing debug logger')
-  console.info(`${count++} Running cron job...`)
-  res.end()
-})
-
-app.get("/user/:email", (req, res) => {
-  // console.info(`[info]: ${JSON.stringify(req.params)}`)
-  const query = "select id, name from users where email = ?"
-  
-  pool.query(query, [req.params.email], (error, results) => {
-    if (!results[0]) {
-      res.json({ status: "not found!" })
-    } else {
-      res.json(results[0])
-    }
-  })
-})
-
-/**
- * connect database
- */
-let configure = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-}
-if (process.env.APP_ENV === 'development') {
-  configure = {...configure, host: process.env.DB_HOST}
-} else {
-  configure = {...configure, socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`}
-}
-const pool = mysql.createPool(configure)
+app.use(routes.home)
+app.use(routes.profile)
+app.use(routes.logging)
 
 /**
  * run application
